92fff513345d01e2e0e552bc5310a3589d7c681e
carlospunkk <cromulo.romulo@gmail.com>
2021-04-23 19:37:39 -0300

    arquivos index java

---
+++ aulaif.js
@@ -0,0 +1,25 @@
+//console.log("aulaif está funcionando")
+/*var vel =11.00
+console.log("a sua velocidade é : "  +vel  )
+if (vel<=10 ) {
+    console.log("você está indo muito devagar")
+
+}else{
+    console.log("voce esta acima da velocidade permitida  ")
+}*/
+
+/*Var pais ='BRASIL'
+
+if (pais =='BRASIL'){
+    console.log("BRASILEIRO   "+pais)
+}else{
+    console.log("ESTRANGEIRO   "+pais)
+}*/
+
+/*function calcular(){
+    var v = window.document.querySelector('txtv1')
+    var res = window.document.querySelector('div#res')
+    var vel = Number(v.value)
+    res.innerHTML("sua velocidade atual é : "+vel)
+
+}*/
